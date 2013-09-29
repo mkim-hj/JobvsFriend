fc6ffb78ae8a15ad86714114f4b6e787664f5ba0
Anand Lakshminarayan <anand.lakshminarayan@gmail.com>
2013-09-28 21:40:46 -0700

    latest back-end update

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,34 @@ app.get("/:uid/:access_token/", function(req,res){
   //get previous duels if any
   //logic to generate duel
   //write new user to table
-
   var url = "https://graph.facebook.com/" + req.params.uid + "/friends?limit=10&access_token=" + req.params.access_token;
-  request(url, function(req,resp,body){
-    console.log(JSON.parse(body)["data"])
-    return resp;
+  request(url, function(re,resp,body){
+
+    //list of friends based on uid and access token
+    friends = JSON.parse(body)["data"];
+
+    //get previous duels
+    query = "SELECT * from duels where player_fb_id = " + req.params.uid;
+    query_db(query,function(result){
+      console.log(result);
+    });
+
+    // get a random friend
+    random_friend = friends[5]["name"]
+    random_salary = 150000
+
+    //(logic) generate duel and send back to front-end
+    var duel = {
+        "company": "Google",
+        "salary": random_salary,
+        "friend" : random_friend,
+        "friendship_duration" : 5,
+        "count": 1
+    }
+
+    res.json(200, duel);
   });
+
 });
 
 app.post("/:uid/:friend/:company/:salary/:result/", function(req,res){
@@ -38,20 +60,22 @@ app.get("/:uid/",function(req,res){
   //get stats from DB and return
 });
 
-function query_db(query_string){
+function query_db(query_string, callback){
     pg.connect(conString, function(err, client, done) {
-    if(err) {
-        return console.error('error fetching client from pool', err);
-    }
-    client.query(query_string, function(err, result) {
-      //call `done()` to release the client back to the pool
-      done();
-
       if(err) {
-        return console.error('error running query', err);
+          console.error('error fetching client from pool', err);
+          return;
       }
-      client.end();
-    });
+      client.query(query_string, function(err, result) {
+        //call `done()` to release the client back to the pool
+        done();
+
+        if(err) {
+          console.error('error running query', err);
+          return;
+        }
+        callback(result);
+      });
   });
 }
 
